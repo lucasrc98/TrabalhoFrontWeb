d6c284698f5c538c6c9166fa78195890a8751fa3
Lucas Rodrigues <lucasrchaves1@gmail.com>
2018-12-17 21:26:17 -0300

    Commit readme.txt

---
+++ js/verMais.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+
+    $("#ParteOculta").hide();
+
+    $('#BotaoParte2').click(function (j) {
+        $("#ParteOculta").show();
+        $(this).hide();
+    })
+});
+
+
+
